2eb3a36db3394404ce47f9153ea193f5eff37b4b
Kled <1017yu@daum.net>
2021-09-02 14:35:08 +0900

    Update dicegame

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -1,11 +1,33 @@
 import ReactDOM from 'react-dom';
+import App from './App';
+
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+const me = 'rock';
+const other = 'scissor';
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
 
 ReactDOM.render(  // Empty Fragment 사용
-  <>  
+  <> 
     <h1 id="title">가위바위보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
+    <h2>{getResult(me, other)}</h2>  
+    <button onClick={handleClick} className="hand">가위</button>
+    <button onClick={handleClick} className="hand">바위</button>
+    <button onClick={handleClick} className="hand">보</button>
+    <App />
   </>
   , document.getElementById('root')
 );
