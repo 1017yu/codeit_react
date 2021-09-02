35539b82ee66b3d1ce6e23ddd8579446f09062dd
Kled <1017yu@daum.net>
2021-09-02 13:28:48 +0900

    Update index.js

--- dicegame/src/index.js
+++ dicegame/src/index.js
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello React!</h1>,
-  document.getElementById('root')
+ReactDOM.render(  // Empty Fragment 사용
+  <>  
+    <h1 id="title">가위바위보</h1>
+    <button className="hand">가위</button>
+    <button className="hand">바위</button>
+    <button className="hand">보</button>
+  </>
+  , document.getElementById('root')
 );
